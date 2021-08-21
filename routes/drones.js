a71f15ded5cf93f59f7ec1dd4162537c48470adb
jlp21 <portelli21@gmail.com>
2021-08-21 17:07:04 -0400

    finished--minimal style

--- routes/drones.js
+++ routes/drones.js
@@ -7,37 +7,86 @@ const Drone = require("../models/Drone.model");
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
   Drone.find()
-  .then(allDronesFromDB => {
-    console.log("drones:", allDronesFromDB);
+    .then(allDronesFromDB => {
+      console.log("drones:", allDronesFromDB);
 
-    res.render("../views/drones/list", {drones: allDronesFromDB, numberOfDrones: allDronesFromDB.length});
-  })
-  .catch(error => console.log("An error occurred while getting books from database: ", error ))
+      res.render("../views/drones/list", {
+        drones: allDronesFromDB,
+        numberOfDrones: allDronesFromDB.length
+      });
+    })
+    .catch(error => console.log("An error occurred while getting drones from database: ", error))
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render("../views/drones/create-form.hbs");
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const {
+    name,
+    propellers,
+    maxSpeed
+  } = req.body;
+
+  Drone.create({
+      name,
+      propellers,
+      maxSpeed
+    })
+    .then(newDrone => {
+
+      res.redirect("/drones")
+    })
+    .catch(error => {
+      console.log("An error occurred while getting drones from database: ", error)
+      res.redirect("/views/drones/create-form.hbs")
+    })
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get('/drones/:id/update', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  Drone.findById(req.params.id)
+    .then(droneToBeEdited => {
+      res.render("drones/update-form", droneToBeEdited)
+    })
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post('/drones/:id/update', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const {
+    name,
+    propellers,
+    maxSpeed
+  } = req.body;
+
+  Drone.findByIdAndUpdate(req.params.id, {
+      name,
+      propellers,
+      maxSpeed
+    }, {
+      new: true
+    })
+    .then(updatedDrone => {
+      console.log("is this updated >>>>> ", updatedDrone);
+
+      res.redirect("/drones");
+    })
+    .catch(error => console.log("An error occurred while updating a drone in the database: ", error));
+
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+
+  Drone.findByIdAndDelete(req.params.id)
+    .then(() => {
+        res.redirect("/drones");
+    })
+    .catch(error => console.log("An error occurred while deleting a drone from the database: ", error ));
+
 });
 
 module.exports = router;
